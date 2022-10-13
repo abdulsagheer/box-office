e3f74c7b4a2f1c6dccbabbae2bb1e079902141ff
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-13 15:38:07 +0530

    Added Header component

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,22 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Movie from "./pages/Movie/Movie";
 import Favorites from "./pages/Favorites/Favorites";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies/:id" element={<Movie />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-      <h1>Box Office</h1>
+      <Header />
       <div>
         <Link to="/">Home</Link>
         <Link to="/movies/:id">Movie</Link>
         <Link to="/favorites">Favorites</Link>
       </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies/:id" element={<Movie />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link to="/">
+          <img
+            className="img-logo"
+            src="https://stur-exchange.co.uk/theme-content/uploads/2021/07/Box-office-logo.png"
+            alt="logo"
+          />
+        </Link>
+        <Link
+          to="/favorites"
+          className="favorites text-right"
+          style={{ textDecoration: "none" }}
+        >
+          Favorites
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
