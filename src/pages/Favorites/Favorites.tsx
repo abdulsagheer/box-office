65ed560d881cd584c5baa9a49b5c4a4733ba1ccf
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-13 19:09:45 +0530

    Added favorites card to favorite page

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../app/hooks";
+import { FiArrowLeft } from "react-icons/fi";
+import Card, { MovieCardData } from "../../components/Card/Card";
+import "./style.css";
 
 const Favorites = () => {
+  const { movies } = useAppSelector((state) => state.favorites);
+  const navigate = useNavigate();
   return (
-    <div>Favorites</div>
-  )
-}
-
-export default Favorites
+    <div className="container">
+      <div className="header">
+        <FiArrowLeft size={24} onClick={() => navigate("/")} />
+      </div>
+      <h4 className="title">Favorites</h4>
+      {!movies.length && (
+        <h3 style={{ marginTop: "70px" }}>Please add your favorite movies</h3>
+      )}
+      <div className="row">
+        {movies?.map(({ id, poster_path, original_title }: MovieCardData) => (
+          <Card
+            key={id}
+            id={id}
+            poster_path={poster_path}
+            original_title={original_title}
+          ></Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Favorites;
