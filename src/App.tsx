486703862077d49a7b1617049f9e7d9dbed9375d
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-13 15:22:07 +0530

    Added Routes for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Movie from "./pages/Movie/Movie";
+import Favorites from "./pages/Favorites/Favorites";
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies/:id" element={<Movie />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
       <h1>Box Office</h1>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/movies">Movie</Link>
+        <Link to="/favorites">Favorites</Link>
+      </div>
     </div>
   );
 }
