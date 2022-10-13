764a894d69cad952c222f094aefc1716ba152807
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-10-13 16:24:44 +0530

    Added card and pagination functionality

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import usePaginatedFetchMovies from "../../services/PaginateMovies";
+import Card, { MovieCardData } from "../../components/Card/Card";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [page, setPage] = useState<number>(1);
+  const { data: movies, isLoading } = usePaginatedFetchMovies(page);
+  console.log("movies =>", movies);
+  return (
+    <>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div className="container">
+          <div className="row">
+            {movies?.map(
+              ({ id, poster_path, original_title }: MovieCardData) => (
+                <Card
+                  id={id}
+                  poster_path={poster_path}
+                  original_title={original_title}
+                ></Card>
+              )
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Home;
